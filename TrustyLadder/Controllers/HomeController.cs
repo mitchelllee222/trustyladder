09f7fba46b2900786bd203858c914436d756285e
Mitch Atchley <mitchelllee222@gmail.com>
2018-11-05 22:09:32 -0600

    Updated layout to use more of the standard layout from Microsoft instead of the one from Devexpress.  It's much cleaner.  Also figured out how to get static webpages to show like a view.

--- TrustyLadder/Controllers/HomeController.cs
+++ TrustyLadder/Controllers/HomeController.cs
@@ -29,5 +29,11 @@ namespace TrustyLadder.Controllers {
         {
             return View();
         }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
     }
 }
