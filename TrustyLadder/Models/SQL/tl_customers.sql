f992a8943a7d4b82ea08eb7b84849a9cf64a7d34
Mitch Atchley <mitchelllee222@gmail.com>
2020-03-23 21:24:15 -0500

    Fixed issue with Customers
    Set customers key ID to auto incrememnt and unqiue to fix issue in which it wasn't adding more than one customer due to duplicate key.

--- TrustyLadder/Models/SQL/tl_customers.sql
+++ TrustyLadder/Models/SQL/tl_customers.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE `tl_customers` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `first_name` varchar(45) DEFAULT NULL,
   `last_name` varchar(45) DEFAULT NULL,
   `business_name` varchar(45) DEFAULT NULL,
@@ -7,6 +7,7 @@
   `address2` varchar(100) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
   `state` varchar(2) DEFAULT NULL,
-  `zip_code` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `zip_code` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- TrustyLadder/Models/SQL/tl_serviceaddresses.sql
+++ TrustyLadder/Models/SQL/tl_serviceaddresses.sql
@@ -1,13 +1,13 @@
-﻿tl_servicesCREATE TABLE `tl_serviceaddresses` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `customerid` int(11) DEFAULT NULL,
+﻿CREATE TABLE `tl_serviceaddresses` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `customerid` int DEFAULT NULL,
   `business_name` varchar(50) DEFAULT NULL,
   `address1` varchar(100) DEFAULT NULL,
   `address2` varchar(100) DEFAULT NULL,
   `city` varchar(50) DEFAULT NULL,
   `state` varchar(2) DEFAULT NULL,
-  `zip_code` int(11) DEFAULT NULL,
+  `zip_code` int DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_tl_serviceaddresses_tl_customers_idx` (`customerid`),
   CONSTRAINT `fk_tl_serviceaddresses_tl_customers` FOREIGN KEY (`customerid`) REFERENCES `tl_customers` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
