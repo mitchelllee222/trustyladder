004818ed44a4785541b65396e9836ee370186b7a
mitchelllee222 <mitchelllee222@gmail.com>
2023-12-27 09:08:19 -0600

    Added support for theming
    Added support for theming from AWS Amplify/Figma.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Amplify } from 'aws-amplify'; // Corrected import
+import { withAuthenticator, ThemeProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
 
-function App() {
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <div>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </div>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
